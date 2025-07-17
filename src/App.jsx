bdf9b9e3d0032d485ed55717607673186d6f7096
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-07-18 00:18:51 +0300

    add styles to the dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './Pages/';
+import Home from './Pages/HomePage/HomePage';
 import Login from './Pages/Login/Login';
 import CreateCapsule from './Pages/CreateCapsule/CreateCapsule';
-import MyCapsules from './Pages/MyCapsules';
+import MyCapsules from './Pages/MyCapsules/MyCapsules';
 import Register from './Pages/Register/Register';
-import Explore from './Pages/Explore';
-import SurpriseMode from './Pages/SurpriseMode';
+import Explore from './Pages/Explore/Explore';
+// import SurpriseMode from './Pages/SurpriseMode';
 import './styles/App.css';
 
 
@@ -18,7 +18,6 @@ function App() {
         <Route path="/create" element={<CreateCapsule />} />
         <Route path="/capsules" element={<MyCapsules />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/surprise" element={<SurpriseMode />} />
 
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
